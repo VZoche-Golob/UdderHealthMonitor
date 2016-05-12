1ca8b71837e96137837ee8bf68f1c718a0dbe17d
VZoche-Golob <VZoche-Golob@users.noreply.github.com>
2016-05-12 16:01:28 +0200

    IndicatorSheet(): Different symbols and line types.

--- R/ui_functions.R
+++ R/ui_functions.R
@@ -3,29 +3,42 @@
 #' This function calculates the most important udder health indicators that are
 #' based on the monthly dairy herd improvement test somatic cell counts (SCC)
 #' according to the DLQ and \url{http://www.milchqplus.de} and produces a concise
-#' graphical report on a single page in pdf-format.
+#' graphical report on a single page in pdf-format.  
+#' The indicators are calculated for the 13 recent months for which data is provided.  
 #'
 #' @param PCstart Character of length 1. Full path to the ADIS file to be used for monitoring.
 #' @param Farm A character string, the 'name' used for the herd / farm in the report.
-#' @out_dir Optional character of length 1. Full path to the directory where the report
+#' @param out_dir Optional character of length 1. Full path to the directory where the report
 #'  file should be produced. If not specified the directory of \code{PCstart} is used.
-#' @out_window Logical of length 1. Should the report be shown in the standard plot display?
+#' @param out_window Logical of length 1. Should the report be shown in the standard plot display?
 #'  (Default is \code{FALSE})
-#' @return_results Logical of length 1. Should the data shown in the report be returned
+#' @param return_results Logical of length 1. Should the data shown in the report be returned
 #'  as a data.frame? (Default is \code{FALSE}).
 #'
-#' @details See \code{\link{prepare_PCstart}} for the required input file.
+#' @details See \code{\link{prepare_PCstart}} for the required input file. The 
+#'   definitions of the indicators are given in the package vignette
+#'   \file{UdderHealthMonitor::User Guide}.
 #'
 #' @return A grafical report in pdf-format is always produced in the \code{out_dir}
-#'  directory. If \code{return_results} is \code{TRUE}, the data is also returned
-#'  as data.frame. \code{NULL} invisible otherwise.
+#'  directory. If \env{return_results = TRUE}, the calculated values of the indicators
+#'  are also returned as a data.frame with one row per month and the columns:
+#'  \describe{
+#'   \item{\code{LowSCC}}{Percentage of cows without mastitis}
+#'   \item{\code{LactNI}}{Lactational new infection rate}
+#'   \item{\code{LactCure}}{Lactational cure rate}
+#'   \item{\code{Chronic}}{Percentage of cows with chronic mastitis}
+#'   \item{\code{NoCure}}{Percentage of cows with low chances of cure}
+#'   \item{\code{DryNI}}{Dry period new infection rate}
+#'   \item{\code{DryCure}}{Dry period cure rate}
+#'   \item{\code{HeiferMast}}{Heifer mastitis rate}
+#'  }
+#'  \code{NULL} invisible otherwise.
 #'
 #' @references Visit \url{http://www.milchqplus.de} for further descriptions
 #'  of the udder health indicators.
 #' @references Have a look at \cite{V. Zoche, W. Heuwieser, and V. Kroemker, "Risk-based monitoring of udder health. A review."}, \href{http://www.schattauer.de/t3page/1214.html?manuscript=16040}{Tierärztliche Praxis. Ausgabe G, Grosstiere/Nutztiere, vol. 39, no. 2, pp. 88–94, 2011.} and \href{http://www.lkvsachsen.de/fileadmin/lkv/redaktion/download/unternehmen/Veredlungsland/Handout_Euterschulung.pdf}{these slides} for information about monitoring the udder health.
 #'
 #' @note Currently different 'AE's are ignored and all animals of one herd are analysed together.
-#' @note This function does not require LaTeX.
 #'
 #' @importFrom magrittr '%>%'
 #'
@@ -55,7 +68,6 @@ IndicatorSheet <- function(
 
   }
   assertive::assert_is_of_length(PCstart, 1)
-  assertive::assert_all_are_readable_files(PCstart)
 
 
   if (is.null(out_dir)) {
@@ -99,7 +111,7 @@ IndicatorSheet <- function(
     period = character(0),
     stringsAsFactors = FALSE
   )
-  for (p in c("Lactation", "DryPeriod")) {
+  for (p in c("Lactation", "Dry Period")) {
 
     if (p == "Lactation") {
 
@@ -134,11 +146,12 @@ IndicatorSheet <- function(
   # plot indicators
 
   i2 <- indicators
+  i2$in2 <- factor(i2$indicator, levels = unique(i2$indicator), labels = "i")
   i2$idx <- rep(13:1, 8)
 
   indPlot <- lattice::xyplot(
     value ~ idx | period,
-    groups = indicator,
+    groups = in2,
     data = i2,
     main = "UdderHealthMonitor-IndicatorSheet",
     sub = paste(Farm, Sys.Date(), sep = "; created: "),
@@ -155,16 +168,30 @@ IndicatorSheet <- function(
     strip = FALSE,
     strip.left = TRUE,
     type = c("g", "b"),
-    lwd = 1.5,
     layout = c(1, 2),
     between = list(
       x = 0,
       y = 0.2
     ),
+    par.settings = simpleTheme(
+      lwd = 1.5,
+      lty = c(1, 2, 3, 4, 6, 2, 3, 5),
+      pch = c(13, 1, 0, 5, 6, 16, 15, 17)
+    ),
     auto.key = list(
+      text = c(
+        "Percentage of cows without mastitis",
+        "Lactational new infection rate",
+        "Lactational cure rate",
+        "Percentage of cows with chronic mastitis",
+        "Percentage of cows with low chances of cure",
+        "Dry period new infection rate",
+        "Dry period cure rate",
+        "Heifer mastitis rate"
+      ),
       points = FALSE,
       lines = TRUE,
-      lwd = 1.5,
+      cex = 0.6,
       type = "b",
       divide = 1,
       columns = 4)
